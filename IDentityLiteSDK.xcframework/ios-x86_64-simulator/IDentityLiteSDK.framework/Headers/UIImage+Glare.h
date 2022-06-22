7460988b2562f0c49a437e3e833ac9340138c831
Amol Deshmukh <amol.deshmukh@idmission.com>
2022-06-22 15:17:14 +0530

    Added UICustomization properties in App

---
+++ IDentityLiteSDK.xcframework/ios-x86_64-simulator/IDentityLiteSDK.framework/Headers/UIImage+Glare.h
@@ -0,0 +1,18 @@
+//
+//  UIImage+Glare.h
+//  IDentitySDK
+//
+//  Created by Stefan Kaczmarek on 6/14/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIImage (Glare)
+
++ (BOOL)isLightCorrect:(UIImage*)uiimage;
+
+@end
+
+NS_ASSUME_NONNULL_END
