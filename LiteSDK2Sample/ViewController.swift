7eeda7bbbf21cf41143f036b65cbf4a027a2ed39
Amol Deshmukh <amol.deshmukh@idmission.com>
2022-09-28 14:08:08 +0530

    Updated SDK v9.3.4.2.8

--- LiteSDK2Sample/ViewController.swift
+++ LiteSDK2Sample/ViewController.swift
@@ -73,6 +73,9 @@ class ViewController: UIViewController {
         IDCapture.options.backRealnessThreshold = 0.3
         IDCapture.options.frontDocumentConfidence = 0.7
         IDCapture.options.backDocumentConfidence = 0.7
+        IDCapture.options.documentComponentConfidence = 0.5
+        IDCapture.options.focusThreshold = 0.95
+//        IDCapture.options.minimumFocusThreshold = 0.75
         IDCapture.options.lowerWidthThresholdTolerance = 0.4
         IDCapture.options.upperWidthThresholdTolerance = 0.1
         IDCapture.options.isDebugMode = false
@@ -81,7 +84,8 @@ class ViewController: UIViewController {
         IDCapture.options.uploadIdData = true
         IDCapture.options.capture4K = true
 
-        //IDCapture Camera Screen Customization
+        //IDCapture Camera Screen UI Customization
+        IDCapture.strings.closeButtonText = "Cancel"
         IDCapture.strings.captureFront = "Capture Front Side"
         IDCapture.strings.captureBack = "Capture Back Side"
         IDCapture.strings.moveAway = "Move ID Away"
@@ -89,20 +93,43 @@ class ViewController: UIViewController {
         IDCapture.strings.makeSurePhotoTextVisible = "Make Sure Photo & Text are Visible in ID"
         IDCapture.strings.makeSureBarcodeVisible = "Make sure Barcode is visible"
         IDCapture.strings.alignRectangle = "Align ID inside Rectangle"
+        IDCapture.strings.notInFocus = "Not In Focus"
         IDCapture.strings.flipToBack = "Flip To Back side"
         IDCapture.strings.tooMuchGlare = "Too Much Glare"
+        IDCapture.colors.overlayTopViewColor = .clear
+        IDCapture.colors.closeButtonTextColor = .white
+        IDCapture.colors.closeButtonColor = .clear
+        IDCapture.colors.closeButtonImageTintColor = .white
         IDCapture.colors.successLabelBackgroundColor = .green
         IDCapture.colors.errorLabelBackgroundColor = .red
         IDCapture.colors.successLabelTextColor = .white
+        IDCapture.colors.captureLabelColor = .clear
+        IDCapture.colors.captureLabelTextColor = .white
         IDCapture.colors.errorLabelTextColor = .white
         IDCapture.colors.backgroundColor = .clear
+        IDCapture.fonts.closeButtonTextFont = UIFont.systemFont(ofSize: 20)
         IDCapture.fonts.captureLabelFont = UIFont.boldSystemFont(ofSize: 14)
         IDCapture.fonts.labelFont = UIFont.systemFont(ofSize: 14)
+        IDCapture.layout.labelPosition = .top
+//        IDCapture.images.closeButtonImage = UIImage(named: "Close")
+//        IDCapture.images.silhouetteImage = UIImage(named: "IdOverlay")
 //        IDCapture.images.frontSilhouetteImage = UIImage(named: "IdFrontOverlay")
 //        IDCapture.images.backSilhouetteImage = UIImage(named: "IdBackOverlay")
-//        IDCapture.images.retryScreenImage = UIImage(named: "IdRetryImage")
+//        IDCapture.images.bottomInfoImage = UIImage(named: "IdiInfo")
 
-        //IDCapture Retry Screen Customization
+        //IDCapture Instruction Screen UI Customization
+        IDCapture.strings.instructionScreenText = "Lorem ipsum text"
+        IDCapture.strings.instructionScreenButtonText = "Continue"
+        IDCapture.colors.instructionScreenBackgroundColor = .white
+        IDCapture.colors.instructionScreenImageTintColor = .white
+        IDCapture.colors.instructionScreenLabelTextColor = .black
+        IDCapture.colors.instructionScreenButtonTextColor = .white
+        IDCapture.colors.instructionScreenButtonBackgroundColor = .gray
+        IDCapture.fonts.instructionScreenLabelFont = UIFont.systemFont(ofSize: 20)
+        IDCapture.fonts.instructionScreenButtonFont = UIFont.systemFont(ofSize: 20)
+//        IDCapture.images.instructionScreenImage = UIImage(named: "IDCaptureImage")
+
+        //IDCapture Retry Screen UI Customization
         IDCapture.strings.retryScreenText = "RealID not Detected. Please try again"
         IDCapture.strings.retryButtonText = "Retry"
         IDCapture.strings.cancelButtonText = "Cancel"
@@ -113,6 +140,7 @@ class ViewController: UIViewController {
         IDCapture.colors.retryScreenButtonBackgroundColor = .clear
         IDCapture.fonts.retryScreenLabelFont = UIFont.systemFont(ofSize: 16)
         IDCapture.fonts.retryScreenButtonFont = UIFont.systemFont(ofSize: 14)
+//        IDCapture.images.retryScreenImage = UIImage(named: "IdRetryImage")
 
         //SelfieCapture Customization
         SelfieCapture.options.minFaceWidth = 0.6
@@ -131,17 +159,18 @@ class ViewController: UIViewController {
         SelfieCapture.options.capture4K = false
         SelfieCapture.options.uploadFaceData = true
 
-        //SelfieCapture Camera Screen Customization
+        //SelfieCapture Camera Screen UI Customization
+        SelfieCapture.strings.closeButtonText = "Cancel"
         SelfieCapture.strings.alignOval = "Align your face inside oval"
-        SelfieCapture.strings.moveAway = "Move ID Away"
-        SelfieCapture.strings.moveCloser = "Move ID Closer"
+        SelfieCapture.strings.moveAway = "Move Face Away"
+        SelfieCapture.strings.moveCloser = "Move Face Closer"
         SelfieCapture.strings.leftEyeClosed = "Left eye are closed"
         SelfieCapture.strings.rightEyeClosed = "right eye are closed"
         SelfieCapture.strings.faceMaskDetected = "Face mask detected"
         SelfieCapture.strings.glassesDetected = "Glasses Detected"
-        SelfieCapture.strings.removeHat = "hat Detected"
+        SelfieCapture.strings.removeHat = "Hat Detected"
         SelfieCapture.strings.fakeFace = "Fake face Detected"
-        SelfieCapture.strings.realFace = "real face Detected"
+        SelfieCapture.strings.realFace = "Real face Detected"
         SelfieCapture.strings.straightenHead = "Make Sure your head is straight"
         SelfieCapture.strings.moveFaceDown = "Move face Down"
         SelfieCapture.strings.moveFaceUp = "Move face Up"
@@ -149,15 +178,34 @@ class ViewController: UIViewController {
         SelfieCapture.strings.capturingFace = "Capturing Face"
         SelfieCapture.strings.tooMuchLight = "Too much light around face"
         SelfieCapture.colors.backgroundColor = .clear
+        SelfieCapture.colors.overlayTopViewColor = .clear
+        SelfieCapture.colors.closeButtonTextColor = .white
+        SelfieCapture.colors.closeButtonColor = .clear
+        SelfieCapture.colors.closeButtonImageTintColor = .white
         SelfieCapture.colors.successLabelBackgroundColor = .green
         SelfieCapture.colors.errorLabelBackgroundColor = .red
         SelfieCapture.colors.successLabelTextColor = .white
         SelfieCapture.colors.errorLabelTextColor = .white
-        SelfieCapture.fonts.labelFont = UIFont.systemFont(ofSize: 14)
-//        SelfieCapture.images.silhouetteImage = UIImage(named: "Selfiecapture")
-//        SelfieCapture.images.retryScreenImage = UIImage(named: "SelfieRetryImage")
+        SelfieCapture.fonts.labelFont = UIFont.systemFont(ofSize: 20)
+        SelfieCapture.fonts.closeButtonTextFont = UIFont.systemFont(ofSize: 20)
+        SelfieCapture.layout.labelPosition = .top
+//        SelfieCapture.images.closeButtonImage = UIImage(named: "Close")
+//        SelfieCapture.images.silhouetteImage = UIImage(named: "SelfieOverlay")
+//        SelfieCapture.images.bottomInfoImage = UIImage(named: "Selfieinfo")
 
-        //SelfieCapture Retry Screen Customization
+        //SelfieCapture Instruction Screen UI Customization
+        SelfieCapture.strings.instructionScreenText = "Lorem ipsum text"
+        SelfieCapture.strings.instructionScreenButtonText = "Continue"
+        SelfieCapture.colors.instructionScreenBackgroundColor = .white
+        SelfieCapture.colors.instructionScreenImageTintColor = .white
+        SelfieCapture.colors.instructionScreenLabelTextColor = .black
+        SelfieCapture.colors.instructionScreenButtonTextColor = .white
+        SelfieCapture.colors.instructionScreenButtonBackgroundColor = .gray
+        SelfieCapture.fonts.instructionScreenLabelFont = UIFont.systemFont(ofSize: 20)
+        SelfieCapture.fonts.instructionScreenButtonFont = UIFont.systemFont(ofSize: 20)
+//        SelfieCapture.images.instructionScreenImage = UIImage(named: "SelfieCaptureImage")
+
+        //SelfieCapture Retry Screen UI Customization
         SelfieCapture.strings.retryScreenText = "Live face not Detected. Please try again"
         SelfieCapture.strings.retryButtonText = "Retry"
         SelfieCapture.strings.cancelButtonText = "Cancel"
@@ -166,8 +214,9 @@ class ViewController: UIViewController {
         SelfieCapture.colors.retryScreenImageTintColor = .blue
         SelfieCapture.colors.retryScreenButtonTextColor = .blue
         SelfieCapture.colors.retryScreenButtonBackgroundColor = .clear
-        SelfieCapture.fonts.retryScreenLabelFont = UIFont.systemFont(ofSize: 16)
-        SelfieCapture.fonts.retryScreenButtonFont = UIFont.systemFont(ofSize: 14)
+        SelfieCapture.fonts.retryScreenLabelFont = UIFont.systemFont(ofSize: 20)
+        SelfieCapture.fonts.retryScreenButtonFont = UIFont.systemFont(ofSize: 20)
+//        SelfieCapture.images.retryScreenImage = UIImage(named: "SelfieRetryImage")
 
     }
 
