3f7835ac1a3245c9b9566f7b755175e018842445
Amol Deshmukh <amol.deshmukh@idmission.com>
2024-11-18 14:24:57 +0530

    Updated SDK v9.6.22.2.9

--- LiteSDK2Sample/ViewController.swift
+++ LiteSDK2Sample/ViewController.swift
@@ -8,61 +8,76 @@
 import UIKit
 import IDentityLiteSDK
 
-class ViewController: UIViewController {
-
-    @IBOutlet var templateURL_Textfield : UITextField!
-    @IBOutlet var apiBaseURL_Textfield : UITextField!
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //Token Generation TextField
+    @IBOutlet var authUrl_Textfield : UITextField!
     @IBOutlet var loginID_Textfield : UITextField!
     @IBOutlet var password_Textfield : UITextField!
     @IBOutlet var merchantID_Textfield : UITextField!
+    @IBOutlet var clientID_Textfield : UITextField!
+    @IBOutlet var clientSecret_Textfield : UITextField!
+    
+    //SDK Initialization TextField
+    @IBOutlet var apiBaseURL_Textfield : UITextField!
+    @IBOutlet var token_Textfield : UITextField!
+    
     @IBOutlet var activityIndicator : UIActivityIndicatorView!
     @IBOutlet var sdkVersion_Label : UILabel!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         sdkVersion_Label.text = "SDK Version : " + IDentitySDK.version
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        token_Textfield.text = ""
+    }
+    
     //Initialize SDK
-    @IBAction func initializeSDK(_ sender:Any) {
+    @IBAction func generateAccessTokenTapped(_ sender:Any) {
+        generateAccessToken()
+    }
+
+    //Initialize SDK
+    @IBAction func initializeSDKTapped(_ sender:Any) {
+        SDKInitializationAPICall()
+    }
+    
+}
+
+//MARK: - SDK Initialization Method
+extension ViewController {
         
-        //Validate the input data
-        guard let templateModelURL = templateURL_Textfield.text , !templateModelURL.isEmpty, !templateModelURL.trimmingCharacters(in: .whitespacesAndNewlines).elementsEqual("") else{
-                //Show alert
-            self.displayAlert(title: "URL Can't Be Empty", Message: "")
+    func SDKInitializationAPICall() {
+        
+        guard let apiBaseUrl = apiBaseURL_Textfield.text, apiBaseUrl != "" else {
+            displayAlert(title: "", Message: "Please enter valid ApiBaseUrl.")
             return
         }
-        
-        guard let baseAPIURLURL = apiBaseURL_Textfield.text , !baseAPIURLURL.isEmpty, !baseAPIURLURL.trimmingCharacters(in: .whitespacesAndNewlines).elementsEqual("") else{
-                //Show alert
-            self.displayAlert(title: "URL Can't Be Empty", Message: "")
+        guard let generatedToken = token_Textfield.text, apiBaseUrl != "" else {
+            displayAlert(title: "", Message: "Please enter valid token.")
             return
         }
-        
-        guard let theLoginID = loginID_Textfield.text , !theLoginID.isEmpty, !theLoginID.trimmingCharacters(in: .whitespacesAndNewlines).elementsEqual("") else{
-                //Show alert
-            self.displayAlert(title: "LoginID Can't Be Empty", Message: "")
-            return
-        }
-        
-        guard let thePassword = password_Textfield.text , !thePassword.isEmpty, !thePassword.trimmingCharacters(in: .whitespacesAndNewlines).elementsEqual("") else{
-                //Show alert
-            self.displayAlert(title: "Password Can't Be Empty", Message: "")
-            return
-        }
-        
-        guard let theMerchantID = merchantID_Textfield.text , !theMerchantID.isEmpty, !theMerchantID.trimmingCharacters(in: .whitespacesAndNewlines).elementsEqual("") else{
-                //Show alert
-            self.displayAlert(title: "MerchantID Can't Be Empty", Message: "")
-            return
-        }
-        
-        //SDK Customization method
+
+        self.activityIndicator.startAnimating()
+
+        //1. Customize SDK properties
         SDKCustomization()
         
-        //SDKInitialization method
-        SDKInitializationAPICall(templateModelURL: templateModelURL, baseAPIURLURL: baseAPIURLURL, theLoginID: theLoginID, thePassword: thePassword, theMerchantID: theMerchantID)
+        //2. initialize SDK
+        IDentitySDK.apiBaseUrl = apiBaseUrl
+        IDentitySDK.initializeSDK(accessToken: generatedToken) { error in
+            self.activityIndicator.stopAnimating()
+            if let error = error {
+                print("!!! initialize SDK ERROR: \(error.localizedDescription)")
+                self.displayAlert(title: "Error", Message: "SDK initialization credentials are not correct")
+            } else {
+                print("!!! initialize SDK SUCCESS")
+                self.performSegue(withIdentifier: "CompleteKYC_SegueID", sender: nil)
+            }
+        }
 
     }
     
@@ -73,17 +88,15 @@ class ViewController: UIViewController {
         IDCapture.options.backRealnessThreshold = 0.3
         IDCapture.options.frontDocumentConfidence = 0.7
         IDCapture.options.backDocumentConfidence = 0.7
-        IDCapture.options.documentComponentConfidence = 0.5
         IDCapture.options.lowerWidthThresholdTolerance = 0.4
         IDCapture.options.upperWidthThresholdTolerance = 0.1
         IDCapture.options.isDebugMode = false
         IDCapture.options.enableInstructionScreen = true
-        IDCapture.options.enableRealId = true
+        IDCapture.options.enableRealId = false
         IDCapture.options.uploadIdData = true
         IDCapture.options.capture4K = true
 
-        //IDCapture Camera Screen UI Customization
-        IDCapture.strings.closeButtonText = "Cancel"
+        //IDCapture Camera Screen Customization
         IDCapture.strings.captureFront = "Capture Front Side"
         IDCapture.strings.captureBack = "Capture Back Side"
         IDCapture.strings.moveAway = "Move ID Away"
@@ -93,40 +106,18 @@ class ViewController: UIViewController {
         IDCapture.strings.alignRectangle = "Align ID inside Rectangle"
         IDCapture.strings.flipToBack = "Flip To Back side"
         IDCapture.strings.tooMuchGlare = "Too Much Glare"
-        IDCapture.colors.overlayTopViewColor = .clear
-        IDCapture.colors.closeButtonTextColor = .white
-        IDCapture.colors.closeButtonColor = .clear
-        IDCapture.colors.closeButtonImageTintColor = .white
         IDCapture.colors.successLabelBackgroundColor = .green
         IDCapture.colors.errorLabelBackgroundColor = .red
         IDCapture.colors.successLabelTextColor = .white
-        IDCapture.colors.captureLabelColor = .clear
-        IDCapture.colors.captureLabelTextColor = .white
         IDCapture.colors.errorLabelTextColor = .white
         IDCapture.colors.backgroundColor = .clear
-        IDCapture.fonts.closeButtonTextFont = UIFont.systemFont(ofSize: 20)
         IDCapture.fonts.captureLabelFont = UIFont.boldSystemFont(ofSize: 14)
         IDCapture.fonts.labelFont = UIFont.systemFont(ofSize: 14)
-        IDCapture.layout.labelPosition = .top
-//        IDCapture.images.closeButtonImage = UIImage(named: "Close")
-//        IDCapture.images.silhouetteImage = UIImage(named: "IdOverlay")
 //        IDCapture.images.frontSilhouetteImage = UIImage(named: "IdFrontOverlay")
 //        IDCapture.images.backSilhouetteImage = UIImage(named: "IdBackOverlay")
-//        IDCapture.images.bottomInfoImage = UIImage(named: "IdiInfo")
-
-        //IDCapture Instruction Screen UI Customization
-        IDCapture.strings.instructionScreenText = "Lorem ipsum text"
-        IDCapture.strings.instructionScreenButtonText = "Continue"
-        IDCapture.colors.instructionScreenBackgroundColor = .white
-        IDCapture.colors.instructionScreenImageTintColor = .white
-        IDCapture.colors.instructionScreenLabelTextColor = .black
-        IDCapture.colors.instructionScreenButtonTextColor = .white
-        IDCapture.colors.instructionScreenButtonBackgroundColor = .gray
-        IDCapture.fonts.instructionScreenLabelFont = UIFont.systemFont(ofSize: 20)
-        IDCapture.fonts.instructionScreenButtonFont = UIFont.systemFont(ofSize: 20)
-//        IDCapture.images.instructionScreenImage = UIImage(named: "IDCaptureImage")
-
-        //IDCapture Retry Screen UI Customization
+//        IDCapture.images.retryScreenImage = UIImage(named: "IdRetryImage")
+        
+        //IDCapture Retry Screen Customization
         IDCapture.strings.retryScreenText = "RealID not Detected. Please try again"
         IDCapture.strings.retryButtonText = "Retry"
         IDCapture.strings.cancelButtonText = "Cancel"
@@ -137,7 +128,6 @@ class ViewController: UIViewController {
         IDCapture.colors.retryScreenButtonBackgroundColor = .clear
         IDCapture.fonts.retryScreenLabelFont = UIFont.systemFont(ofSize: 16)
         IDCapture.fonts.retryScreenButtonFont = UIFont.systemFont(ofSize: 14)
-//        IDCapture.images.retryScreenImage = UIImage(named: "IdRetryImage")
 
         //SelfieCapture Customization
         SelfieCapture.options.minFaceWidth = 0.6
@@ -156,18 +146,17 @@ class ViewController: UIViewController {
         SelfieCapture.options.capture4K = false
         SelfieCapture.options.uploadFaceData = true
 
-        //SelfieCapture Camera Screen UI Customization
-        SelfieCapture.strings.closeButtonText = "Cancel"
+        //SelfieCapture Camera Screen Customization
         SelfieCapture.strings.alignOval = "Align your face inside oval"
-        SelfieCapture.strings.moveAway = "Move Face Away"
-        SelfieCapture.strings.moveCloser = "Move Face Closer"
+        SelfieCapture.strings.moveAway = "Move ID Away"
+        SelfieCapture.strings.moveCloser = "Move ID Closer"
         SelfieCapture.strings.leftEyeClosed = "Left eye are closed"
         SelfieCapture.strings.rightEyeClosed = "right eye are closed"
         SelfieCapture.strings.faceMaskDetected = "Face mask detected"
         SelfieCapture.strings.sunglassesDetected = "Glasses Detected"
-        SelfieCapture.strings.removeHat = "Hat Detected"
+        SelfieCapture.strings.removeHat = "hat Detected"
         SelfieCapture.strings.fakeFace = "Fake face Detected"
-        SelfieCapture.strings.realFace = "Real face Detected"
+        SelfieCapture.strings.realFace = "real face Detected"
         SelfieCapture.strings.straightenHead = "Make Sure your head is straight"
         SelfieCapture.strings.moveFaceDown = "Move face Down"
         SelfieCapture.strings.moveFaceUp = "Move face Up"
@@ -175,34 +164,15 @@ class ViewController: UIViewController {
         SelfieCapture.strings.capturingFace = "Capturing Face"
         SelfieCapture.strings.tooMuchLight = "Too much light around face"
         SelfieCapture.colors.backgroundColor = .clear
-        SelfieCapture.colors.overlayTopViewColor = .clear
-        SelfieCapture.colors.closeButtonTextColor = .white
-        SelfieCapture.colors.closeButtonColor = .clear
-        SelfieCapture.colors.closeButtonImageTintColor = .white
         SelfieCapture.colors.successLabelBackgroundColor = .green
         SelfieCapture.colors.errorLabelBackgroundColor = .red
         SelfieCapture.colors.successLabelTextColor = .white
         SelfieCapture.colors.errorLabelTextColor = .white
-        SelfieCapture.fonts.labelFont = UIFont.systemFont(ofSize: 20)
-        SelfieCapture.fonts.closeButtonTextFont = UIFont.systemFont(ofSize: 20)
-        SelfieCapture.layout.labelPosition = .top
-//        SelfieCapture.images.closeButtonImage = UIImage(named: "Close")
-//        SelfieCapture.images.silhouetteImage = UIImage(named: "SelfieOverlay")
-//        SelfieCapture.images.bottomInfoImage = UIImage(named: "Selfieinfo")
+        SelfieCapture.fonts.labelFont = UIFont.systemFont(ofSize: 14)
+//        SelfieCapture.images.silhouetteImage = UIImage(named: "Selfiecapture")
+//        SelfieCapture.images.retryScreenImage = UIImage(named: "SelfieRetryImage")
 
-        //SelfieCapture Instruction Screen UI Customization
-        SelfieCapture.strings.instructionScreenText = "Lorem ipsum text"
-        SelfieCapture.strings.instructionScreenButtonText = "Continue"
-        SelfieCapture.colors.instructionScreenBackgroundColor = .white
-        SelfieCapture.colors.instructionScreenImageTintColor = .white
-        SelfieCapture.colors.instructionScreenLabelTextColor = .black
-        SelfieCapture.colors.instructionScreenButtonTextColor = .white
-        SelfieCapture.colors.instructionScreenButtonBackgroundColor = .gray
-        SelfieCapture.fonts.instructionScreenLabelFont = UIFont.systemFont(ofSize: 20)
-        SelfieCapture.fonts.instructionScreenButtonFont = UIFont.systemFont(ofSize: 20)
-//        SelfieCapture.images.instructionScreenImage = UIImage(named: "SelfieCaptureImage")
-
-        //SelfieCapture Retry Screen UI Customization
+        //SelfieCapture Retry Screen Customization
         SelfieCapture.strings.retryScreenText = "Live face not Detected. Please try again"
         SelfieCapture.strings.retryButtonText = "Retry"
         SelfieCapture.strings.cancelButtonText = "Cancel"
@@ -211,31 +181,11 @@ class ViewController: UIViewController {
         SelfieCapture.colors.retryScreenImageTintColor = .blue
         SelfieCapture.colors.retryScreenButtonTextColor = .blue
         SelfieCapture.colors.retryScreenButtonBackgroundColor = .clear
-        SelfieCapture.fonts.retryScreenLabelFont = UIFont.systemFont(ofSize: 20)
-        SelfieCapture.fonts.retryScreenButtonFont = UIFont.systemFont(ofSize: 20)
-//        SelfieCapture.images.retryScreenImage = UIImage(named: "SelfieRetryImage")
+        SelfieCapture.fonts.retryScreenLabelFont = UIFont.systemFont(ofSize: 16)
+        SelfieCapture.fonts.retryScreenButtonFont = UIFont.systemFont(ofSize: 14)
 
     }
 
-    func SDKInitializationAPICall(templateModelURL:String, baseAPIURLURL:String, theLoginID:String, thePassword:String, theMerchantID:String) {
-        
-        self.activityIndicator.startAnimating()
-
-        IDentitySDK.initializeApiBaseUrl = templateModelURL
-        IDentitySDK.apiBaseUrl = baseAPIURLURL
-        IDentitySDK.initializeSDK(loginId: theLoginID, password: thePassword, merchantId: theMerchantID) { error in
-            self.activityIndicator.stopAnimating()
-            if let error = error {
-                print("!!! initialize SDK ERROR: \(error.localizedDescription)")
-                self.displayAlert(title: "Error", Message: "SDK initialization credentials are not correct")
-            } else {
-                print("!!! initialize SDK SUCCESS")
-                self.performSegue(withIdentifier: "CompleteKYC_SegueID", sender: nil)
-            }
-        }
-
-    }
-    
     func displayAlert(title : String, Message:String){
         let alert = UIAlertController(title: title, message: Message, preferredStyle: .alert)
 
@@ -243,4 +193,18 @@ class ViewController: UIViewController {
         alert.addAction(okAction)
         self.present(alert, animated: true)
     }
+}
+
+//MARK: - UITextfield Delegate Method
+extension ViewController {
+    
+    override func resignFirstResponder() -> Bool {
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
 }
